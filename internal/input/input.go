ae2a8ad17cf6ffbf0f2202713e71d84d50670381
Tom <tom@tdpain.net>
2021-01-20 21:51:24 +0000

    Add internal/input package

---
+++ internal/input/input.go
@@ -0,0 +1,66 @@
+package input
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+func Prompt(prompt string) string {
+	fmt.Print(prompt)
+	scanner.Scan()
+	r := scanner.Text()
+	return r
+}
+
+func PromptMulti(prompt string) (items []string) {
+	fmt.Println(prompt)
+	fmt.Println("(Enter a blank line to continue)")
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		inputted := scanner.Text()
+
+		if inputted == "" {
+			fmt.Println()
+			return
+		}
+
+		items = append(items, inputted)
+	}
+
+}
+
+func PromptSelect(prompt string, options []string) (selected int, chosenItem string) {
+	fmt.Println(prompt)
+	for i, v := range options {
+		fmt.Printf("  %d: %s\n", i+1, v)
+	}
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		userSelected, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Not an integer.")
+			continue
+		}
+
+		userSelected -= 1
+
+		if userSelected < 0 || userSelected >= len(options) {
+			fmt.Println("Out of bounds.")
+			continue
+		}
+
+		selected = userSelected
+		chosenItem = options[userSelected]
+
+		fmt.Println()
+
+		return
+	}
+}
