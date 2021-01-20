36762b76c96a568bed0670a28887d9bc7a0dd294
Tom <tom@tdpain.net>
2021-01-20 19:59:56 +0000

    Add template decoding and loading

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,5 +1,50 @@
 package data
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
 //go:generate go-bindata -pkg data -prefix "dataFiles/" ./dataFiles/...
 
-func X(){}
+type Template struct {
+	Name string
+	Key  string
+
+	Vars        map[string]string
+	Directories []string
+	Files       map[string]string
+	Commands    []struct {
+		Command string
+		Stdin   []string
+	}
+}
+
+const templateDirName = "templates"
+
+var AvailableTemplates []*Template
+
+func init() {
+	list, err := AssetDir(templateDirName)
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range list {
+		tpl, err := LoadTemplate(strings.Join([]string{templateDirName, f}, "/"))
+		if err != nil {
+			panic(fmt.Errorf("data.init: (%s) %s", f, err.Error()))
+		}
+		AvailableTemplates = append(AvailableTemplates, tpl)
+	}
+}
+
+func LoadTemplate(templateName string) (*Template, error) {
+	fCont, err := Asset(templateName)
+	if err != nil {
+		return nil, err
+	}
+
+	loadedTemplate := new(Template)
+	return loadedTemplate, json.Unmarshal(fCont, loadedTemplate)
+}
